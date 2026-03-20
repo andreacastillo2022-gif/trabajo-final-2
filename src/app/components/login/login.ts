fcd39214359dfaf1d2dbe92e4ad3238e01cb5867
Andrea Agustina Castillo <andreacastillo2022@gmail.com>
2026-03-19 23:40:09 -0300

    estilos css

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
-export class Login {}
+
+export class Login {
+
+ usuario = new FormControl('');
+  password = new FormControl('');
+
+  login() {
+    console.log('Usuario:', this.usuario.value);
+    console.log('Password:', this.password.value);
+  
+} }
