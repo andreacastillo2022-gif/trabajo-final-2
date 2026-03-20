af8be701e8baac093ee438e51202139317eaff52
Andrea Agustina Castillo <andreacastillo2022@gmail.com>
2026-03-20 02:14:08 -0300

    mas cambios css, app

--- src/app/components/chat/chat.ts
+++ src/app/components/chat/chat.ts
@@ -13,7 +13,7 @@ export class Chat {
   {
     id: 1,
     nombre: 'Luna',
-    avatar: '',
+    avatar: 'https://st2.depositphotos.com/4967775/11238/v/450/depositphotos_112387970-stock-illustration-avatar-girls-icon-vector-woman.jpg',
     mensajes: [
       { texto: 'Holaaaaaa', mio: false },
       { texto: 'Holii', mio: true }
@@ -22,10 +22,12 @@ export class Chat {
   {
     id: 2,
     nombre: 'Andrea',
-    avatar: '',
+    avatar: 'https://st2.depositphotos.com/4967775/11255/v/450/depositphotos_112551512-stock-illustration-avatar-girls-icon-vector-woman.jpg',
     mensajes: [
       { texto: 'Todo bien', mio: false },
-      { texto: 'Buenos dias', mio: true }
+      { texto: 'Buenos dias', mio: true },
+      { texto: 'Como estas?', mio: false },
+      { texto: 'Contestaaa', mio: false },
     ]
   }
   ]
