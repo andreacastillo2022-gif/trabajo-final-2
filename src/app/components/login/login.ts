d48d0b76fa6147cc66bbb156388bba54fe0825f9
Andrea Agustina Castillo <andreacastillo2022@gmail.com>
2026-03-20 17:25:15 -0300

    login acceso con usuarios

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -11,26 +11,46 @@ import { Router } from '@angular/router';
 })
 
 
-export class Login {
-
- usuario = new FormControl('');
+export class Login implements OnInit { 
+  
+  usuario = new FormControl('');
   password = new FormControl('');
 
-  login() {
-    console.log('Usuario:', this.usuario.value);
-    console.log('Password:', this.password.value);
+ngOnInit() {
 
-    if (!this.usuario.value || !this.password.value) {
-      alert('Completá los campos');
-      return;
+  const usuariosGuardados = localStorage.getItem('usuarios');
+  const data = localStorage.getItem('usuarios');
+
+  if (!usuariosGuardados) {
+    const usuarios = [
+      { username: 'andrea', password: '1234' },
+      { username: 'luna', password: '1234' }
+    ];
+
+    localStorage.setItem('usuarios', JSON.stringify(usuarios));
+  }
+
+  }
+
+  login() {
+
+    const data = localStorage.getItem('usuarios');
+    const usuarios = data ? JSON.parse(data) : [];
+
+    const encontrado = usuarios.find(
+      (u: any) =>
+        u.username === this.usuario.value &&
+        u.password === this.password.value
+    );
+
+    if (encontrado) {
+      localStorage.setItem('usuarioActivo', JSON.stringify(encontrado));
+      window.location.href = '/chat';
+    }
+    else {
+      alert('Usuario o contraseña incorrectos');
+
     }
 
-    localStorage.setItem('usuario', this.usuario.value);
 
-    this.router.navigate(['/chat']);
-
-    };
-  constructor(private router: Router) {}
-
-
-} 
+}}
